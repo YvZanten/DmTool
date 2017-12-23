a135c08ad7da133b9de1f1f6df1e34ae1b2228f2
YvZanten <yeroenvz@gmail.com>
2017-12-23 20:25:03 +0100

    Zoom works
    Zooming in and out now keeps moiuse on same world position

--- Assets/scripts/CameraZoom.cs
+++ Assets/scripts/CameraZoom.cs
@@ -25,11 +25,21 @@ public class CameraZoom : MonoBehaviour
         float ScrollDelta = Input.mouseScrollDelta.y;
         if(Input.GetKey(KeyCode.LeftControl) && ScrollDelta != 0)
         {
+            //get target location of mouse
+            Vector3 v3_Target = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            //change camera size
             float CamSize = cam.orthographicSize - ScrollDelta;
             cam.orthographicSize = Mathf.Max(SizeMin, Mathf.Min(SizeMax, CamSize));
 
+            //redraw lines
             if (GridManager != null)
                 GridManager.GetComponent<DrawLines>().RedrawGrid();
+
+            //get new mouse position, get difference, and move camera to keep mouse at same world position
+            Vector3 v3_Source = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 v3_Diff = v3_Source - v3_Target;
+            cam.transform.position -= new Vector3(v3_Diff.x, v3_Diff.y);
         }
     }
 }
