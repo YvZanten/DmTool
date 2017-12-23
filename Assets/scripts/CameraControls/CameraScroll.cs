491ede3b80372555384da06f6ee70e33b134395d
YvZanten <yeroenvz@gmail.com>
2017-12-23 20:33:47 +0100

    Scroll function added
    can now xcroll vertically and horizontally

---
+++ Assets/scripts/CameraControls/CameraScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour
+{
+    private Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float ScrollDelta = Input.mouseScrollDelta.y;
+        if(ScrollDelta != 0)
+        {
+            bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+            if(!shift)
+                cam.transform.position += new Vector3(0, ScrollDelta * 1.5f);
+            else
+                cam.transform.position += new Vector3(ScrollDelta * -1.5f, 0);
+        }
+	}
+}
