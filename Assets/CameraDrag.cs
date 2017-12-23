f258d2c628f1d80c2e2e1e1329e606f0e39bcfee
YvZanten <yeroenvz@gmail.com>
2017-12-23 16:41:07 +0100

    First Version
    Drawlines
    First version of dragging

---
+++ Assets/CameraDrag.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    //imports
+    public float CamSpeed;
+
+    //local
+    private Camera cam;
+
+    private Vector3 MousePos;
+    private Vector3 CameraPos;
+    private bool CamDrag;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cam = GetComponent<Camera>();
+
+        MousePos = new Vector3();
+        CamDrag = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if M1 down
+		if(Input.GetMouseButtonDown(0))
+        {
+            MousePos = cam.ScreenToWorldPoint(Input.mousePosition);     //save mouse position
+            CameraPos = cam.transform.position; //save cam position
+            CamDrag = true;                     //start dragging
+        }
+
+        //if M1 up
+        if (Input.GetMouseButtonUp(0))
+        {
+            CamDrag = false;    //stop dragging
+        }
+
+        //if dragging
+        if(CamDrag)
+        {
+            Vector3 MouseWorldPos = cam.ScreenToWorldPoint(Input.mousePosition) - MousePos * -CamSpeed;
+            cam.transform.position = (CameraPos + MouseWorldPos);    //move camera
+        }
+    }
+}
---
+++ Assets/DrawLines.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+    //imports
+    public Camera GameCamera;
+    
+    public GameObject GridLine;
+
+    public int GridWidth;
+    public int GridHeight;
+
+    //local
+    private const int GridCellSize = 64;
+
+    private LineRenderer[] GridArray_Vertical;
+    private LineRenderer[] GridArray_Horizontal;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.unityLogger.Log("shop", GridWidth.ToString());
+
+        //create lines
+        Debug.unityLogger.Log("", "got width as thick as " + GridWidth);
+        CreateLines();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //Create LineRenderers
+    private void CreateLines()
+    {
+        //create vertical lines
+        GridArray_Vertical = new LineRenderer[GridWidth];
+        for (int index = 0; index < GridWidth; index++)
+        {
+            GameObject line = Instantiate(GridLine);
+            LineRenderer lr = line.GetComponent<LineRenderer>();
+            GridArray_Vertical[index] = lr;
+
+            //position size already set to 2 by Prefab
+            lr.SetPosition(0, GameCamera.ScreenToWorldPoint(
+                new Vector3(
+                    (index - GridWidth / 2 + 1) * GridCellSize, 
+                    -(GridHeight / 2 * GridCellSize), 
+                    -(GameCamera.transform.position.z)
+                    )));
+
+            lr.SetPosition(1, GameCamera.ScreenToWorldPoint(
+                new Vector3(
+                    (index - GridWidth / 2 + 1) * GridCellSize, 
+                    +(GridHeight / 2 * GridCellSize), 
+                    -(GameCamera.transform.position.z)
+                    )));
+
+            line.transform.parent = this.transform;
+        }
+
+        //create horizontal lines
+        GridArray_Horizontal = new LineRenderer[GridWidth];
+        for (int index = 0; index < GridHeight; index++)
+        {
+            GameObject line = Instantiate(GridLine);
+            LineRenderer lr = line.GetComponent<LineRenderer>();
+            GridArray_Horizontal[index] = lr;
+
+            //position size already set to 2 by Prefab
+            lr.SetPosition(0, GameCamera.ScreenToWorldPoint(
+                new Vector3(
+                    -(GridWidth / 2 * GridCellSize), 
+                    (index - GridHeight / 2 + 1) * GridCellSize, 
+                    -(GameCamera.transform.position.z)
+                    )));
+
+            lr.SetPosition(1, GameCamera.ScreenToWorldPoint(
+                new Vector3(
+                    +(GridWidth / 2 * GridCellSize), 
+                    (index - GridHeight / 2 + 1) * GridCellSize, 
+                    -(GameCamera.transform.position.z)
+                    )));
+
+            line.transform.parent = this.transform;
+        }
+    }
+}
