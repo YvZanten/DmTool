e8a04871d75d0dd5ac6bedab06f2e78803d7c4a7
Timon van Zanten <t.d.vanzanten@students.uu.nl>
2017-12-26 21:40:17 +0100

    menu setup

---
+++ Assets/scripts/ToolManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolManager : MonoBehaviour {
+    
+    public enum ToolType { Grab, Rectangle};
+    public ToolType CurrentTooltype;
+    public Camera SceneCamera;
+
+    private CameraDrag cd;
+
+	// Use this for initialization
+	void Start () {
+        CurrentTooltype = ToolType.Grab;
+        cd = SceneCamera.GetComponent<CameraDrag>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeToolType(int tool)
+    {
+        CurrentTooltype = (ToolType)tool;
+        Debug.Log(tool.ToString());
+    }
+}
